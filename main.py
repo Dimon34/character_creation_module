ebcc50bb537af6f413ab67a86f827b13ead4aa8f
Dimon34 <36888338+Dimon34@users.noreply.github.com>
2023-01-24 21:56:42 +0300

    Did the task

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ SPECIAL_SKILL = 'Удача'
 SPECIAL_BUFF = 15
 DEFAULT_STAMINA = 80
 
+
 class Character:
     '''Принимает вводимые игроком Имя и Класс'''
 
@@ -17,7 +18,6 @@ class Character:
 
     def __init__(self, name):
         self.name = name
-
 
     def attack(self):
         value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
@@ -43,6 +43,7 @@ class Warrior(Character):
     SPECIAL_BUFF = DEFAULT_STAMINA + 25
     SPECIAL_SKILL = 'Выносливость'
 
+
 class Mage(Character):
     BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
                              'Обладает высоким интеллектом')
@@ -50,6 +51,7 @@ class Mage(Character):
     RANGE_VALUE_DEFENCE = (-2, 2)
     SPECIAL_BUFF = DEFAULT_ATTACK + 40
     SPECIAL_SKILL = 'Атака'
+
 
 class Healer(Character):
     BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
@@ -59,5 +61,48 @@ class Healer(Character):
     SPECIAL_BUFF = DEFAULT_DEFENCE + 30
     SPECIAL_SKILL = 'Защита'
 
+
 def choice_char_class(char_name: str) -> Character:
-    pass
+    """
+    Возвращает строку с выбранным
+    классом персонажа.
+    """
+
+    game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
+
+    approve_choice: str = None
+
+    while approve_choice != 'y':
+        selected_class = input('Введи название персонажа, '
+                               'за которого хочешь играть: Воитель — warrior, '
+                               'Маг — mage, Лекарь — healer: ')
+        char_class: Character = game_classes[selected_class](char_name)
+        print(char_class)
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа ').lower()
+    return start_training(char_class)
+
+
+def start_training(char_class: object) -> Character:
+    cmd: str = None
+    print('Потренируйся управлять своими навыками.')
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special — чтобы использовать свою суперсилу.')
+    print('Если не хочешь тренироваться, введи команду skip.')
+    commands = {'attack': char_class.attack,
+                'defence': char_class.defence,
+                'special': char_class.special}
+
+    while cmd != 'skip':
+        cmd = input('Введите команду ')
+        if cmd in commands:
+            print(commands[cmd]())
+        elif cmd not in commands:
+            continue
+
+    return print('Тренировка окончена.')
+
+
+choice_char_class(input('Введи свое имя '))
